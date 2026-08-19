05930c04c845c12309f0d54798a39664b3f9e9c5
GabrielAGES <gaguilar004@soyudemedellin.edu.co>
2026-08-19 14:29:34 -0500

    Jugador
    falta usar el transform para el movimiento

--- P_A_POO/Assets/Scripts/Jugador.cs
+++ P_A_POO/Assets/Scripts/Jugador.cs
@@ -2,5 +2,34 @@ using UnityEngine;
 
 public class Jugador : Personaje
 {
+    // Atributos propios del jugador
+    private int energia = 100;
+    private int nivel = 1;
+    private bool tocoCura = false;
 
+    // Métodos propios del jugador
+    void Update()
+    {
+        //direccion con teclas
+        float direccion = Input.GetAxisRaw("Horizontal");
+        Mover(direccion);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Saltar();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Atacar();
+        }
+    }
+    public void Atacar()
+    {
+        Debug.Log(Nombre + "ataca");
+    }
+
+    public void RecogerObjeto()
+    {
+        Debug.Log(Nombre + " recogió");
+    }
 }
