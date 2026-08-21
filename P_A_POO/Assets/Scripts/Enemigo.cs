58a7d8d148aeb3b166511068c37a05b4e1154873
GabrielAGES <gaguilar004@soyudemedellin.edu.co>
2026-08-21 13:10:02 -0500

    Enemigo
    El enemigo patrulla y genera daño al jugador

--- P_A_POO/Assets/Scripts/Enemigo.cs
+++ P_A_POO/Assets/Scripts/Enemigo.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private string tipo;
+    
+    public float speed = 2f;
+    public Transform[] points;
+
+    private int i;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, points[i].position) < 0.25f)
+        {
+            i++;
+
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
     }
 }
--- P_A_POO/Assets/Scripts/PlataformaMovil.cs
+++ P_A_POO/Assets/Scripts/PlataformaMovil.cs
@@ -24,10 +24,22 @@ public class PlataformaMovil : MonoBehaviour
             }
         }
 
-        transform.position = Vector2.MoveTowards(
-            transform.position,
-            points[i].position,
-            speed * Time.deltaTime
-        );
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(null);
+        }
     }
 }
